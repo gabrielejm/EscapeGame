4d85e1225d1acd40c4deb4fcd272fcfa5a7bad25
gudetama <daniellewwise@gmail.com>
2021-02-04 20:03:08 -0500

    merging main with danielle branch

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react'
+import ReactDOM from "react-dom";
+import CanvasDraw from "react-canvas-draw";
+
 
 const Main = () => {
-    return(
+    return (
+      <div>
+        <h1>Welcome to our Escape Game</h1>
+        <p>Play below!</p>
         <div>
-            <h1>Welcome to our Escape Game</h1>
-            <p>Play below!</p>
+          <CanvasDraw
+            imgSrc="https://images-na.ssl-images-amazon.com/images/I/41yjh1X2ahL._AC_.jpg"
+            hideInterface true
+          />
         </div>
-    )
+      </div>
+    );
 }
 
 export default Main
