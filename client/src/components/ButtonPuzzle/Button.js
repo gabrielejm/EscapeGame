13cdfac4b9ed5a39b275a4932651d309e3784ec0
Cmac <mckoyc@gmail.com>
2021-02-11 21:04:54 -0500

    added semantic language to button and submit components

--- client/src/components/ButtonPuzzle/Button.js
+++ client/src/components/ButtonPuzzle/Button.js
@@ -3,13 +3,16 @@ import './ButtonPuzzle.css'
 import {PuzzleContext} from './PuzzleContext'
 
 const Button = ({button}) => {
+    //Calls Puzzle Context
     const puzzle = useContext(PuzzleContext)
+    //Grabs Color from Context
     const color = puzzle.puzzleAttributes[button]
 
+    //Calls dispatch from Reducer to change button color
     const toggle = () => {
         puzzle.dispatch(button)
-        console.log('button', button)
     }
+    //Returns Component
     return (
         <button className="button" onClick={toggle} style={{backgroundColor: color}}></button>
     )
--- client/src/components/ButtonPuzzle/submit.js
+++ client/src/components/ButtonPuzzle/submit.js
@@ -2,8 +2,12 @@ import React, { useContext } from 'react'
 import {PuzzleContext} from './PuzzleContext'
 
 const Submit = () => {
+    //Calls Puzzle Context
     const puzzle = useContext(PuzzleContext)
+    //Pulls Button Colors from Context
     const {button1, button2, button3, button4, button5} = puzzle.puzzleAttributes
+    
+    //Checks to see if the current buttons colors are correct
     const submitAnswer = (()=> {
         const correctAnswer = ['green', 'red', 'red', 'green', 'green']
         if(button1 === correctAnswer[0] &&
