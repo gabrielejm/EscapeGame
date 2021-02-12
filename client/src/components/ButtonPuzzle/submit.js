7b0b64948044d0f8f5d5e0ebfa4d9f8ff33bfdf5
Cmac <mckoyc@gmail.com>
2021-02-11 20:44:32 -0500

    submit button correctly returns correct answers

--- client/src/components/ButtonPuzzle/submit.js
+++ client/src/components/ButtonPuzzle/submit.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {PuzzleContext} from './PuzzleContext'
 
 const Submit = () => {
+    const puzzle = useContext(PuzzleContext)
+    const {button1, button2, button3, button4, button5} = puzzle.puzzleAttributes
     const submitAnswer = (()=> {
         const correctAnswer = ['green', 'red', 'red', 'green', 'green']
-        if()
+        if(button1 === correctAnswer[0] &&
+           button2 === correctAnswer[1] &&
+           button3 === correctAnswer[2] &&
+           button4 === correctAnswer[3] &&
+           button5 === correctAnswer[4]){
+               alert("You are correct")
+           } else {
+               alert("Nope")
+           }
     })
     return (
         <button className="submit" onClick = {submitAnswer}>Submit</button>
