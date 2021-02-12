75d0211161ee943b56fd06ad137003b9bf70ea6e
Cmac <mckoyc@gmail.com>
2021-02-11 21:42:20 -0500

    added modal component folder

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Modal = () => {
+    const image = 'https://www.pngfind.com/pngs/m/377-3773029_9-blank-scroll-banner-png-for-free-download.png'
+
+    const modalStyle = {
+        
+    }
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Modal
