4813f97e3fe23c50e0888dfd2fbc237603d574ea
gudetama <daniellewwise@gmail.com>
2021-02-16 20:30:31 -0500

    moved the matchingame div

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -3,7 +3,6 @@ import "./main.css";
 import React, { useState, useEffect } from "react";
 import Gamescreen from "../components/gamescreen/gamescreen";
 import ButtonPuzzle from "../components/ButtonPuzzle/ButtonPuzzle";
-
 // 3 games
 // if completed, return true and update global state?
 // 1/3 completed displayed on screen
@@ -15,15 +14,16 @@ const Main = () => {
 
   return (
     <div>
-      <div className="container">
-        <h1>Matching Game</h1>
-      </div>
-      <MatchingGame />
       <div id="mainDiv">
         <h1>Welcome to our Escape Game</h1>
         <p>Play below!</p>
         <Gamescreen />
       </div>
+      <div className="container">
+        <h1>Matching Game</h1>
+      </div>
+      <MatchingGame />
+
     </div>
   )
 
