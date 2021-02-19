82e9491813c209e434da9164328f6b487d343349
Cmac <mckoyc@gmail.com>
2021-02-18 19:44:03 -0500

    moved modal context up one level and added dispatch to gamescreen

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -2,6 +2,7 @@ import "./main.css";
 import React, { useState, useEffect } from "react";
 import Gamescreen from "../components/gamescreen/gamescreen";
 import GameContextProvider from '../contexts/gameContext'
+import ModalContextProvider from "../components/Modal/ModalContext";
 // 3 games
 // if completed, return true and update global state?
 // 1/3 completed displayed on screen
@@ -16,9 +17,11 @@ const Main = () => {
       <div id="mainDiv">
         <h1>Welcome to our Escape Game</h1>
         <p>Play below!</p>
+        <ModalContextProvider>
         <GameContextProvider>
           <Gamescreen />
         </GameContextProvider>
+        </ModalContextProvider>
       </div>
     </div>
   )
