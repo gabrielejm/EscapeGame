cc483d59f7194810e30e72e52753da089e8a02fc
Cmac <mckoyc@gmail.com>
2021-02-19 15:14:09 -0500

    added semantic language and deleted console logs in Riddle Submit button component

--- client/src/components/RiddlePuzzle/Submit.js
+++ client/src/components/RiddlePuzzle/Submit.js
@@ -4,16 +4,23 @@ import {GameContext} from '../../contexts/gameContext'
 import {RiddleContext} from "./RiddleContext"
 
 const Submit = () => {
+    // Defines Riddle Context
     const riddle = useContext(RiddleContext)
+    // Defines Modal Context
     const modal = useContext(ModalContext)
+    // Defines Game Context
     const game = useContext(GameContext)
-
+    // Destructures Letters from Riddle Context
     const {letterOne, letterTwo, letterThree, letterFour, letterFive} = riddle.riddleAttributes
 
+    // Checks to see if answer is correct
     const submitAnswer = () => {
+        // Defines the Answer
         const correctAnswer = "dadde"
+        // Creates word from All Letters
         const word = letterOne + letterTwo + letterThree + letterFour + letterFive
 
+        //Confirms Correct Answer and either Completes Puzzle or Sends User back to main screen
         if (correctAnswer === word) {
             document.getElementById('modal').style.visibility = "visible"
             document.getElementById('riddle').style.visibility = "hidden"
@@ -24,10 +31,9 @@ const Submit = () => {
             document.getElementById('riddle').style.visibility = "hidden"
             modal.dispatch({type: "change", value: "You didn't do it :( "})   
         }
-        
-        console.log (word, correctAnswer)
     }
 
+    // Defines HTML of Button
     return(
         <button className = "funcButton" id = "submit" onClick = {submitAnswer}>Submit</button>
     )
