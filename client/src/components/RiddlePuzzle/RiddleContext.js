928e3dede8ec5730079ddc87f4540c5ca476ebd5
Cmac <mckoyc@gmail.com>
2021-02-19 14:58:04 -0500

    added semantic language to riddleContext

--- client/src/components/RiddlePuzzle/RiddleContext.js
+++ client/src/components/RiddlePuzzle/RiddleContext.js
@@ -3,13 +3,16 @@ import {riddleReducer} from './RiddleReducer';
 //Exports Context
 export const RiddleContext = createContext();
 
+//Exports Component
 export const RiddleContextProvider = props => {
+    // directory of letters for answer
     const letters = ['a', 'b', 'c', 'd', 'e']
 
+    //Randomly generates letters for the button
     const randomLetter = (arr) =>{
         return arr[Math.floor(Math.random()* Math.floor(arr.length))]
     }
-    
+    // Randomly generates the button's value before being given to buttons
     const boxes = {
         letterOne : randomLetter(letters),
         letterTwo : randomLetter(letters),
@@ -18,7 +21,9 @@ export const RiddleContextProvider = props => {
         letterFive : randomLetter(letters),
     }
 
+    // sets up the reducer to use riddleReducer and the state to be equal to boxes
     const [riddleAttributes, dispatch] = useReducer(riddleReducer, boxes)
+    // exports the Provider to pass props down to children components
     return <RiddleContext.Provider value = {{riddleAttributes, dispatch}}>{props.children}</RiddleContext.Provider>;
 
 }
