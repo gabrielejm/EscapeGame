b3bdf86c7bf7f4e540b3152b321b4cc3f22b2eac
Ben Cummings <bncmmngs@aol.com>
2021-02-18 19:50:59 -0500

    scoreboard changes

---
+++ client/src/components/scoreboard/scoreboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export class ScoreBoard extends Component {
+    return () {
+        <div>
+            <div className ="card">
+                <div className="card-body">
+                    <table className="table table-borderless">
+                        <tbody>
+                            <tr>
+                                <td className="border-0">name</td>
+                                <td className="border-0">score</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+     </div>
+    )
+    }
