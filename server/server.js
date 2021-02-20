20fcf431e76413543d33711a6bf3a8dc68ffd9e5
gudetama <daniellewwise@gmail.com>
2021-02-20 14:13:53 -0500

    uncommented dependencies

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const mongoose = require('mongoose')
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
