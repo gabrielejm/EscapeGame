2d8ff565e1ff9b0f0961094ce4c7a84aaf189527
gudetama <daniellewwise@gmail.com>
2021-02-20 14:16:59 -0500

    added timer component files

--- client/src/components/timer/timer.js
+++ client/src/components/timer/timer.js
@@ -1,33 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "./timer.css"
 
-const Timer = () => {
-  const { initialMinute = 0, initialSeconds = 0 } = props;
-  const [minutes, setMinutes] = useState(initialMinute);
-  const [seconds, setSeconds] = useState(initialSeconds);
-  useEffect(() => {
-    let interval = setInterval(() => {
-      if (seconds > 0) {
-        setSeconds(seconds - 1);
-      }
-      if (seconds === 0) {
-        if (minutes === 0) {
-          clearInterval(interval);
-        } else {
-          setMinutes(minutes - 1);
-          setSeconds(59);
-        }
-      }
-    }, 1000)
-    return () => {
-      clearInterval(interval);
-    }
-  });
+
+
+export const Timer = () => {
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(30);
+
+  
+ const startTimer = () => {
+   let interval = setInterval(() => {
+     if (seconds > 0) {
+       setSeconds(seconds - 1);
+     }
+     if (seconds === 0) {
+       if (minutes === 0) {
+         clearInterval(interval);
+       } else {
+         setMinutes(minutes - 1);
+         setSeconds(59);
+       }
+     }
+   }, 1000);
+   return () => {
+     clearInterval(interval);
+   };
+ };
+
+  useEffect(() => { startTimer
+    // let interval = setInterval(() => {
+    //   if (seconds > 0) {
+    //     setSeconds(seconds - 1);
+    //   }
+    //   if (seconds === 0) {
+    //     if (minutes === 0) {
+    //       clearInterval(interval);
+    //     } else {
+    //       setMinutes(minutes - 1);
+    //       setSeconds(59);
+    //     }
+    //   }
+    // }, 1000)
+    // return () => {
+    //   clearInterval(interval);
+    // }
+  }, [startGameButton]);
 
   return (
-    <div>
-      {/* ternary: condition ? ifTrueDoThis : ifFalseDoThis */}
+    <div class="timer">
+      {/* ternary expression: condition ? ifTrueDoThis : ifFalseDoThis */}
       {minutes === 0 && seconds === 0
-        ? null : <h1>{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+        ? null : <h2>{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h2>
       }
     </div>
   );
