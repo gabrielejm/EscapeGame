4a39e00e942bf944a569c033e201aac1aa9ddb74
gudetama <daniellewwise@gmail.com>
2021-02-22 17:18:36 -0500

    setUp useEffect and useStates, removed button

--- client/src/components/timer/timer.js
+++ client/src/components/timer/timer.js
@@ -1,58 +1,55 @@
 import React, { useEffect, useState } from "react";
 import "./timer.css"
-import {startGameButton} from "../StartModal/StartModal"
 
 
 
-export const Timer = () => {
+export const Timer = ({ isActive, setIsActive }) => {
   const [minutes, setMinutes] = useState(1);
-  const [seconds, setSeconds] = useState(30);
+  const [seconds, setSeconds] = useState(45);
+  const [counter, setCounter] = useState(0)
+
+  console.log("is the timer activated?", isActive)
+
+  useEffect(() => {
+    let interval
+    if (isActive) {
+      interval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+          console.log("seconds - 1", seconds)
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+          clearInterval(interval)
+          } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+          }
+        }
+        setCounter(counter => counter + 1);
+      }, 1000);
+        if (minutes === 0 && seconds === 0 && isActive === true) {
+        console.log("game over!");
+        setIsActive(false);
+      }
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, counter])
+
+
+
 
   
- const startTimer = () => {
-   let interval = setInterval(() => {
-     if (seconds > 0) {
-       setSeconds(seconds - 1);
-     }
-     if (seconds === 0) {
-       if (minutes === 0) {
-         clearInterval(interval);
-       } else {
-         setMinutes(minutes - 1);
-         setSeconds(59);
-       }
-     }
-   }, 1000);
-   return () => {
-     clearInterval(interval);
-   };
- };
-
-  useEffect(() => { startTimer
-    // let interval = setInterval(() => {
-    //   if (seconds > 0) {
-    //     setSeconds(seconds - 1);
-    //   }
-    //   if (seconds === 0) {
-    //     if (minutes === 0) {
-    //       clearInterval(interval);
-    //     } else {
-    //       setMinutes(minutes - 1);
-    //       setSeconds(59);
-    //     }
-    //   }
-    // }, 1000)
-    // return () => {
-    //   clearInterval(interval);
-    // }
-  }, [startGameButton]);
 
   return (
     <div class="timer">
       {/* ternary expression: condition ? ifTrueDoThis : ifFalseDoThis */}
-      {minutes === 0 && seconds === 0
-        ? null : <h2>{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h2>
+      {<h2>{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h2>
       }
+      {/* !isActive will negate the current isActive value and return true if currently false and vice versa */}
+      {/* <button onClick={() => setIsActive(!isActive)} className="start">{isActive ? "stop" : "start"}</button> */}
     </div>
   );
 };
