a8445e178f09df49b5ff019d5e8fcb4badf22cdd
gudetama <daniellewwise@gmail.com>
2021-02-23 16:24:28 -0500

    added timerCon props

--- client/src/components/timer/TimerContext.js
+++ client/src/components/timer/TimerContext.js
@@ -4,8 +4,12 @@ import timerReducer from "./timerReducer"
 export const TimerContext = createContext();
 
 export const TimerContextProvider = props => {
-
-  const isActive = { active: false };
+  const isActive = {
+    active: false,
+    minutes: 10,
+    seconds: 0,
+    isFinished: false
+  };
   const [timerAttributes, dispatch] = useReducer(timerReducer, isActive)
 
    return <TimerContext.Provider value={{ timerAttributes, dispatch }}>{props.children}</TimerContext.Provider>;
