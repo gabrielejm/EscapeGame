a46fd2786a4e1ced3cc732c185ef47e8758b5b92
gudetama <daniellewwise@gmail.com>
2021-02-23 16:23:36 -0500

    switched out useState logic for context api logic

--- client/src/components/timer/timer.js
+++ client/src/components/timer/timer.js
@@ -5,34 +5,33 @@ import { TimerContext } from "../Timer/TimerContext";
 
 export const Timer = () => {
   const timerCon = useContext(TimerContext)
-  const {active} = timerCon.timerAttributes
-  const [minutes, setMinutes] = useState(10);
-  const [seconds, setSeconds] = useState(0);
+  const {active, minutes, seconds} = timerCon.timerAttributes
+  // const [minutes, setMinutes] = useState(10);
+  // const [seconds, setSeconds] = useState(0);
   const [counter, setCounter] = useState(0)
-
-  // console.log("is the timer activated?", isActive)
 
   useEffect(() => {
     let interval
     if (active) {
       interval = setInterval(() => {
         if (seconds > 0) {
-          setSeconds(seconds - 1);
+          timerCon.dispatch({ type: "seconds", value: seconds - 1 });
           // console.log("seconds - 1", seconds)
         }
         if (seconds === 0) {
           if (minutes === 0) {
           clearInterval(interval)
           } else {
-          setMinutes(minutes - 1);
-          setSeconds(59);
+          timerCon.dispatch({ type: 'minutes', value: minutes - 1 });
+          timerCon.dispatch({type: 'seconds', value: 59});
           }
         }
         setCounter(counter => counter + 1);
       }, 1000);
         if (minutes === 0 && seconds === 0 && active === true) {
-        console.log("game over!");
-        timerCon.dispatch("end")
+          console.log("game over!");
+          timerCon.dispatch({ type: "gameOver" })
+          document.getElementById("FinishModal").style.visibility = "visible";
       }
     }
     return () => {
