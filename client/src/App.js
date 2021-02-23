7eaebbe94a29f969c9124fba278ab6aacda71eab
Justin Gabriele <gabrielejustin@yahoo.com>
2021-02-23 12:24:57 -0500

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Main from "./pages/main";
 import ScoreBoard from "./components/ScoreBoard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Main />
-      {/* <ScoreBoard /> */}
+      <ScoreBoard />
     </div>
   );
 };
