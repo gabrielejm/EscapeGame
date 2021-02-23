bea96bde0eddb92bc41c02a1a389f3b4955addd2
Justin Gabriele <gabrielejustin@yahoo.com>
2021-02-23 12:17:05 -0500

    errors from merge fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
 import Main from "./pages/main";
-// import ScoreBoard from '../components/ScoreBoard'
-import ScoreBoard from "./components/Scoreboard";
+import ScoreBoard from "./components/ScoreBoard";
 
 const App = () => {
   return (
