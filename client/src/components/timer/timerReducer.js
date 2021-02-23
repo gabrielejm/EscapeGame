51bd5165640d38bc5005254d7e78a9d60c3d30b8
gudetama <daniellewwise@gmail.com>
2021-02-23 16:26:13 -0500

    add cases for win + lose scenario and game score

--- client/src/components/timer/timerReducer.js
+++ client/src/components/timer/timerReducer.js
@@ -1,9 +1,17 @@
 export const timerReducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case 'activate':
-      return { active: true }
-    case 'end': 
-      return { active: false }
+      return { ...state, active: true }
+    case 'gameOver': 
+      return { ...state, active: false }
+    case 'youWin':
+      return {...state, active: false, isFinished: true}
+    case 'seconds':
+      return {
+        ...state, seconds: action.value
+      }
+    case 'minutes': 
+      return { ...state, minutes: action.value }
     default:
       return state;
   }
