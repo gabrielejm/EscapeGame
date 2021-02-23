56972ce5d14b79d6b409dae3bce0304cbcd7f19d
gudetama <daniellewwise@gmail.com>
2021-02-22 20:28:09 -0500

    deleted commented import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Main from "./pages/main";
-// import ScoreBoard from '../components/ScoreBoard'
 import ScoreBoard from "./components/Scoreboard";
 
 const App = () => {
