b8271375739980c4d4243ff37b59e61400e7bc95
Justin Gabriele <gabrielejustin@yahoo.com>
2021-02-23 16:11:57 -0500

    navbar done

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,40 +1,11 @@
 // import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-// const NavbarStyle = {
-//   color: "red",
-//   margin: "5px",
-// };
-
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar">
-//       {/* <h1>Escape Game</h1> */}
-//       <div className="links">
-//         <Link style={NavbarStyle} to="/">
-//           Play
-//         </Link>
-//         <Link style={NavbarStyle} to="/scoreboard">
-//           Scoreboard
-//         </Link>
-//         <Link style={NavbarStyle} to="/demo">
-//           Puzzle Demos
-//         </Link>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
-// import MenuIcon from "@material-ui/icons/Menu";
+import "./Navbar.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -53,17 +24,23 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar id="nav-style" position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            <Link to="/">Play</Link>
+            <Link className="links" to="/">
+              Play
+            </Link>
           </Typography>
 
           <Typography variant="h6" className={classes.title}>
-            <Link to="/scoreboard">Scoreboard</Link>
+            <Link className="links" to="/scoreboard">
+              Scoreboard
+            </Link>
           </Typography>
           <Typography variant="h6" className={classes.title}>
-            <Link to="/demo">Puzzle Demos</Link>
+            <Link className="links" to="/demo">
+              Puzzle Demos
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
