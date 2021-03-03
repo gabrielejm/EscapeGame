a253babe556621320a3703c2c2d163445dd9c57e
gudetama <daniellewwise@gmail.com>
2021-03-03 02:07:20 -0500

    removed unused css file import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Main from "./pages/main";
 import ScoreBoard from "./components/scoreboard/Scoreboard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
